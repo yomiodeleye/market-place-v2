4e1cb5ef339d0cb2545d97396881ed11c7368726
Abayomi Odeleye <abayomiodeleye@Abayomis-MacBook-Pro.local>
2023-05-31 00:04:48 +0100

    working on Authentication

--- src/app/components/Breadcrumb/BreadcrumbOne.tsx
+++ src/app/components/Breadcrumb/BreadcrumbOne.tsx
@@ -1,12 +1,27 @@
-import { Container, Row, Col } from "react-bootstrap";
-import clsx from "clsx";
+'use client'
 
-const BreadcrumbOne = ({ children, backgroundImage, pageTitle, className }) => {
+import { ReactNode } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import clsx from 'clsx'
+
+interface BreadcrumbOneProps {
+  children: ReactNode
+  backgroundImage?: string
+  pageTitle?: string
+  className?: string
+}
+
+const BreadcrumbOne = ({
+  children,
+  backgroundImage,
+  pageTitle,
+  className,
+}: BreadcrumbOneProps) => {
   return (
     <div
-      className={clsx("breadcrumb-area space-pt--70 space-pb--70", className)}
+      className={clsx('breadcrumb-area space-pt--70 space-pb--70', className)}
       style={{
-        backgroundImage: `url("${process.env.PUBLIC_URL + backgroundImage}")`
+        backgroundImage: `url("${process.env.PUBLIC_URL + backgroundImage}")`,
       }}
     >
       <Container>
@@ -18,7 +33,7 @@ const BreadcrumbOne = ({ children, backgroundImage, pageTitle, className }) => {
         </Row>
       </Container>
     </div>
-  );
-};
+  )
+}
 
-export default BreadcrumbOne;
+export default BreadcrumbOne
--- src/app/components/header/default-header.tsx
+++ src/app/components/header/default-header.tsx
@@ -57,170 +57,158 @@ const Header = ({ aboutOverlay = false }: { aboutOverlay: boolean }) => {
   return (
     <Fragment>
       <header
-        className={clsx(
-          'topbar-shadow transparent-style white-content',
-          scroll > headerTop && 'is-sticky',
-        )}
+        className={clsx('topbar-shadow', scroll > headerTop && 'is-sticky')}
       >
-        <div className="multilevel-header">
-          <Container className="wide">
-            <Row className="multilevel-header__top">
-              <Col lg={4} className="d-none d-lg-block">
-                <div className="d-flex">
-                  <div className="language-change change-dropdown">
-                    <span>English</span> <IoIosArrowDown />
-                    <ul>
-                      <li>
-                        <button>English</button>
-                      </li>
-                      <li>
-                        <button>Deustch</button>
-                      </li>
-                    </ul>
-                  </div>
-                  <span className="header-separator">|</span>
-                  <div className="currency-change change-dropdown">
-                    <span>USD</span> <IoIosArrowDown />
-                    <ul>
-                      <li>
-                        <button>USD</button>
-                      </li>
-                      <li>
-                        <button>EUR</button>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </Col>
-              <Col xs={6} lg={4} className="text-start text-lg-center">
-                {/* logo */}
-                <div className="header-content__logo">
-                  <Anchor href="/" as={process.env.PUBLIC_URL + '/'}>
-                    <img
-                      src={process.env.PUBLIC_URL + '/assets/images/logo.png'}
-                      className="img-fluid dark-logo"
-                      alt=""
-                    />
-                    <img
-                      src={
-                        process.env.PUBLIC_URL + '/assets/images/logo-alt.png'
-                      }
-                      className="img-fluid white-logo"
-                      alt=""
-                    />
-                  </Anchor>
-                </div>
-              </Col>
-              <Col xs={6} lg={4} className="text-end">
-                <div className="header-content__icons">
-                  <ul className="d-none d-lg-block">
+        <Container className="wide">
+          <Row className="multilevel-header__top">
+            <Col lg={4} className="d-none d-lg-block">
+              <div className="d-flex">
+                <div className="language-change change-dropdown">
+                  <span>English</span> <IoIosArrowDown />
+                  <ul>
                     <li>
-                      <button
-                        onClick={() => {
-                          setOffCanvasSearchActive(true)
-                          // @ts-ignore
-                          document
-                            .querySelector('body')
-                            .classList.add('overflow-hidden')
-                        }}
-                      >
-                        <IoIosSearch />
-                      </button>
+                      <button>English</button>
                     </li>
                     <li>
-                      <Anchor path="/other/login-register">
-                        <IoMdPerson />
-                      </Anchor>
-                    </li>
-                    <li>
-                      <button
-                        onClick={() => {
-                          setOffCanvasWishlistActive(true)
-                          // @ts-ignore
-                          document
-                            .querySelector('body')
-                            .classList.add('overflow-hidden')
-                        }}
-                      >
-                        <IoIosHeartEmpty />
-                        {wishlistItems.length >= 1 ? (
-                          <span className="count">
-                            {wishlistItems.length ? wishlistItems.length : ''}
-                          </span>
-                        ) : (
-                          ''
-                        )}
-                      </button>
-                    </li>
-                    <li>
-                      <button
-                        onClick={() => {
-                          setOffCanvasCartActive(true)
-                          // @ts-ignore
-                          document
-                            .querySelector('body')
-                            .classList.add('overflow-hidden')
-                        }}
-                      >
-                        <IoIosCart />
-                        {cartItems.length >= 1 ? (
-                          <span className="count">
-                            {cartItems.length ? cartItems.length : ''}
-                          </span>
-                        ) : (
-                          ''
-                        )}
-                      </button>
+                      <button>Deustch</button>
                     </li>
                   </ul>
+                </div>
+                <span className="header-separator">|</span>
+                <div className="currency-change change-dropdown">
+                  <span>USD</span> <IoIosArrowDown />
+                  <ul>
+                    <li>
+                      <button>USD</button>
+                    </li>
+                    <li>
+                      <button>EUR</button>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </Col>
+            <Col xs={6} lg={4} className="text-start text-lg-center">
+              {/* logo */}
+              <div className="header-content__logo">
+                <Anchor href="/" as={process.env.PUBLIC_URL + '/'}>
+                  <img
+                    src={process.env.PUBLIC_URL + '/assets/images/logo.png'}
+                    className="img-fluid dark-logo"
+                    alt=""
+                  />
+                  <img
+                    src={process.env.PUBLIC_URL + '/assets/images/logo-alt.png'}
+                    className="img-fluid white-logo"
+                    alt=""
+                  />
+                </Anchor>
+              </div>
+            </Col>
+            <Col xs={6} lg={4} className="text-end">
+              <div className="header-content__icons">
+                <ul className="d-none d-lg-block">
+                  <li>
+                    <button
+                      onClick={() => {
+                        setOffCanvasSearchActive(true)
+                        // @ts-ignore
+                        document
+                          .querySelector('body')
+                          .classList.add('overflow-hidden')
+                      }}
+                    >
+                      <IoIosSearch />
+                    </button>
+                  </li>
+                  <li>
+                    <Anchor path="/other/login-register">
+                      <IoMdPerson />
+                    </Anchor>
+                  </li>
+                  <li>
+                    <button
+                      onClick={() => {
+                        setOffCanvasWishlistActive(true)
+                        // @ts-ignore
+                        document
+                          .querySelector('body')
+                          .classList.add('overflow-hidden')
+                      }}
+                    >
+                      <IoIosHeartEmpty />
+                      {wishlistItems.length >= 1 ? (
+                        <span className="count">
+                          {wishlistItems.length ? wishlistItems.length : ''}
+                        </span>
+                      ) : (
+                        ''
+                      )}
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      onClick={() => {
+                        setOffCanvasCartActive(true)
+                        // @ts-ignore
+                        document
+                          .querySelector('body')
+                          .classList.add('overflow-hidden')
+                      }}
+                    >
+                      <IoIosCart />
+                      {cartItems.length >= 1 ? (
+                        <span className="count">
+                          {cartItems.length ? cartItems.length : ''}
+                        </span>
+                      ) : (
+                        ''
+                      )}
+                    </button>
+                  </li>
+                </ul>
 
-                  <ul className="d-block d-lg-none">
-                    <li>
-                      <Anchor path="/other/wishlist">
-                        <IoIosHeartEmpty />
-                        {wishlistItems.length >= 1 ? (
-                          <span className="count">
-                            {wishlistItems.length ? wishlistItems.length : ''}
-                          </span>
-                        ) : (
-                          ''
-                        )}
-                      </Anchor>
-                    </li>
-                    <li>
-                      <Anchor path="/other/cart">
-                        <IoIosCart />
-                        {cartItems.length >= 1 ? (
-                          <span className="count">
-                            {cartItems.length ? cartItems.length : ''}
-                          </span>
-                        ) : (
-                          ''
-                        )}
-                      </Anchor>
-                    </li>
-                    <li>
-                      <button
-                        onClick={() => setOffCanvasMobileMenuActive(true)}
-                      >
-                        <IoIosMenu />
-                      </button>
-                    </li>
-                  </ul>
-                </div>
-              </Col>
-            </Row>
-            <Row className="multilevel-header__bottom">
-              <Col
-                lg={12}
-                className="text-center d-none d-lg-block space-mt--40"
-              >
-                {/* navigation */}
-                <Navigation />
-              </Col>
-            </Row>
-          </Container>
-        </div>
+                <ul className="d-block d-lg-none">
+                  <li>
+                    <Anchor path="/other/wishlist">
+                      <IoIosHeartEmpty />
+                      {wishlistItems.length >= 1 ? (
+                        <span className="count">
+                          {wishlistItems.length ? wishlistItems.length : ''}
+                        </span>
+                      ) : (
+                        ''
+                      )}
+                    </Anchor>
+                  </li>
+                  <li>
+                    <Anchor path="/other/cart">
+                      <IoIosCart />
+                      {cartItems.length >= 1 ? (
+                        <span className="count">
+                          {cartItems.length ? cartItems.length : ''}
+                        </span>
+                      ) : (
+                        ''
+                      )}
+                    </Anchor>
+                  </li>
+                  <li>
+                    <button onClick={() => setOffCanvasMobileMenuActive(true)}>
+                      <IoIosMenu />
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </Col>
+          </Row>
+          <Row className="multilevel-header__bottom">
+            <Col lg={12} className="text-center d-none d-lg-block space-mt--40">
+              {/* navigation */}
+              <Navigation />
+            </Col>
+          </Row>
+        </Container>
       </header>
 
       {/* about overlay */}
