65aba63d9412c08aa721e1c347c43780c771b3ac
yomiodeleye <odeleye.yomi@!me.com>
2023-06-02 22:53:50 +0100

    fix(header components): fix header dropdown menu

--- components/header/components/header-menu.tsx
+++ components/header/components/header-menu.tsx
@@ -22,53 +22,43 @@ const HeaderMenu = ({
               key={menuOne.id}
             >
               <Link href={menuOne.href}>{menuOne.title}</Link>
-              {/*{menuOne.submenuCName && !menuOne.megamenuCName && (*/}
-              {/*  <ul className={`${menuOne.submenuCName}`}>*/}
-              {/*    {menuOne?.headerSubmenu?.map(*/}
-              {/*      (submenuOne) => (*/}
-              {/*        <li key={submenuOne.id}>*/}
-              {/*          <Link href={`${submenuOne.submenuPath}`}>*/}
-              {/*            {submenuOne.submenuTitle}*/}
-              {/*          </Link>*/}
-              {/*        </li>*/}
-              {/*      )*/}
-              {/*    )}*/}
-              {/*  </ul>*/}
-              {/*)}*/}
-              {/*{menuOne.megamenuCName && !menuOne.submenuCName && (*/}
-              {/*  <ul className={`${menuOne.megamenuCName} flex`}>*/}
-              {/*    {menuOne?.headerMegamenu?.map(*/}
-              {/*      (megamenuOne) => (*/}
-              {/*        <li className="basis-[22%] px-[15px]" key={megamenuOne.id}>*/}
-              {/*                                  <span className="mb-[20px] block font-medium">*/}
-              {/*                                      {megamenuOne.groupName}*/}
-              {/*                                  </span>*/}
-              {/*          <ul>*/}
-              {/*            {megamenuOne?.groupItems?.map(*/}
-              {/*              (groupItem) => (*/}
-              {/*                <li*/}
-              {/*                  className="mb-[10px] last:mb-0"*/}
-              {/*                  key={*/}
-              {/*                    groupItem.id*/}
-              {/*                  }*/}
-              {/*                >*/}
-              {/*                  <Link*/}
-              {/*                    href={`${groupItem.megamenuPath}`}*/}
-              {/*                    className="font-normal transition-all hover:text-primary"*/}
-              {/*                  >*/}
-              {/*                    {*/}
-              {/*                      groupItem.megamenuTitle*/}
-              {/*                    }*/}
-              {/*                  </Link>*/}
-              {/*                </li>*/}
-              {/*              )*/}
-              {/*            )}*/}
-              {/*          </ul>*/}
-              {/*        </li>*/}
-              {/*      )*/}
-              {/*    )}*/}
-              {/*  </ul>*/}
-              {/*)}*/}
+              {menuOne.submenuCName && !menuOne.megamenuCName && (
+                <ul className={`${menuOne.submenuCName}`}>
+                  {menuOne?.headerSubmenu?.map((submenuOne) => (
+                    <li key={submenuOne.id}>
+                      <Link href={`${submenuOne.submenuPath}`}>
+                        {submenuOne.submenuTitle}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              )}
+              {menuOne.megamenuCName && !menuOne.submenuCName && (
+                <ul className={`${menuOne.megamenuCName} flex`}>
+                  {menuOne?.headerMegamenu?.map((megamenuOne) => (
+                    <li className="basis-[22%] px-[15px]" key={megamenuOne.id}>
+                      <span className="mb-[20px] block font-medium">
+                        {megamenuOne.groupName}
+                      </span>
+                      <ul>
+                        {megamenuOne?.groupItems?.map((groupItem) => (
+                          <li
+                            className="mb-[10px] last:mb-0"
+                            key={groupItem.id}
+                          >
+                            <Link
+                              href={`${groupItem.megamenuPath}`}
+                              className="font-normal transition-all hover:text-primary"
+                            >
+                              {groupItem.megamenuTitle}
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </li>
+                  ))}
+                </ul>
+              )}
             </li>
           ))}
         </ul>
